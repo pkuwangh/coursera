98e1b451de9843aa09765a435d939b06807d0a42
Hao Wang <pkuwangh@gmail.com>
2017-05-29 17:13:46 -0500

    [ml-ex6] support vector machines

--- machine-learning-ex6/dataset3Params.m
+++ machine-learning-ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+err_min = -1;
+choices = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+for C_idx = 1 : length(choices)
+    C_t = choices(C_idx);
+    for sigma_idx = 1 : length(choices)
+        sigma_t = choices(sigma_idx);
+        model = svmTrain(X, y, C_t, @(x1, x2) gaussianKernel(x1, x2, sigma_t));
+        predictions = svmPredict(model, Xval);
+        err = mean(double(predictions ~= yval));
+        fprintf("Examining C=\t%f, sigma=\t%f: err=\t%f\n", C_t, sigma_t, err);
+        if (err_min < 0 || err < err_min)
+            err_min = err;
+            C = C_t;
+            sigma = sigma_t;
+            fprintf("Picking C=\t%f, sigma=\t%f, err\t%f\n", C, sigma, err_min);
+        end
+    end
+end
 
 % =========================================================================
 
